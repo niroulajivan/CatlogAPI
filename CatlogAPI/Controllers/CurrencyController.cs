91ba9ac3e50cdcf6723ba55c9b88338d03c6080d
jivan91 <nirjibz.dmk@gmail.com>
2019-04-28 08:06:18 +0545

    Added with required structures..

---
+++ CatlogAPI/Controllers/CurrencyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatlogAPI.Controllers
+{
+    [Route("api/Currency")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        // GET api/currency
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "USD", "NPR", "AUD" };
+        }
+    }
+}
---
+++ OrdersAPI/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrdersAPI.Controllers
+{
+    [Route("api/Orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        // GET api/Orders
+        [HttpGet, Route("")]
+        public IActionResult GetOrders()
+        {
+            var o1 = new Order("Order1", 205);
+            var o2 = new Order("Order2",350);
+            return Ok(
+                new List<Order> { o1, o2 });
+        }
+
+    }
+    public class Order
+    {
+        public string Id { get; set; }
+        public decimal Amount { get; set; }
+        public Order(string id, decimal amount)
+        {
+            Id = id;
+            Amount = amount;
+        }
+    }
+}
